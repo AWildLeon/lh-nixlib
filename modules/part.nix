ff5784b73c768b285688b5c632dfc24b517f72c1
Leon Hubrich <mail@leon-hubrich.de>
2026-06-07 13:15:20 +0200

    Modules Structure

--- modules/part.nix
+++ modules/part.nix
@@ -1,7 +1,24 @@
 _: {
   # Aggregate NixOS module exposing all of lh-nixlib's modules.
   # Add module files to `imports` as they are created.
-  flake.nixosModule.default = {
-    imports = [ ];
+  flake = {
+    imports = [
+      ./profiles/part.nix
+    ];
+
+    nixosModule.default = {
+      imports = [
+        ./cosmetic
+        ./hardware
+        ./networking
+        ./programs
+        ./services
+        ./security
+        ./services
+        ./system
+        ./virtualization
+        ./hardware
+      ];
+    };
   };
 }
